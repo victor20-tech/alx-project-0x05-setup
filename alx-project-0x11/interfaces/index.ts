b39fd6c5b435204b6d3797585628d0d9e9ef1666
victor20-tech <victoradomako10@gmail.com>
2025-10-17 15:43:57 +0000

    Feat: Implement state tracking to store and display generated image history

---
+++ alx-project-0x11/interfaces/index.ts
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+export interface ReactComponentProps {
+    children: React.ReactNode
+}
+
+export interface GeneratedImageProps {
+  imageUrl: string
+  prompt: string
+  width?: string
+  height?: string
+  action: (imagePath: string) => void
+}
+
+export type RequestProps = {
+  prompt: string;
+}
+export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
